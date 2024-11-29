57ae5ee58fb93e438887dd221338749027580236
Fenhl <fenhl@fenhl.net>
2024-11-29 02:43:18 +0000

    More robust impl of IsNetworkError for io::Error using new Rust 1.83 API

--- crate/wheel/src/traits.rs
+++ crate/wheel/src/traits.rs
@@ -529,8 +529,15 @@ impl IsNetworkError for Error {
 
 impl IsNetworkError for io::Error {
     fn is_network_error(&self) -> bool {
-        matches!(self.kind(), io::ErrorKind::BrokenPipe | io::ErrorKind::ConnectionAborted | io::ErrorKind::ConnectionRefused | io::ErrorKind::ConnectionReset | io::ErrorKind::TimedOut | io::ErrorKind::UnexpectedEof)
-        || self.to_string() == "Network is unreachable (os error 101)" //TODO this should be a match on io::ErrorKind::NetworkUnreachable, but it is currently unstable, making it impossible to match against. See https://github.com/rust-lang/rust/issues/86442
+        matches!(self.kind(),
+            | io::ErrorKind::BrokenPipe
+            | io::ErrorKind::ConnectionAborted
+            | io::ErrorKind::ConnectionRefused
+            | io::ErrorKind::ConnectionReset
+            | io::ErrorKind::NetworkUnreachable
+            | io::ErrorKind::TimedOut
+            | io::ErrorKind::UnexpectedEof
+        )
     }
 }
 
